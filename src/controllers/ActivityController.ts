893db25cddbf7bd5f348f1035c15634e4e22eff3
Jota Stefani <jota.stefani@hotmail.com>
2024-07-05 19:29:40 -0300

    add all end point

--- src/controllers/ActivityController.ts
+++ src/controllers/ActivityController.ts
@@ -8,25 +8,71 @@ const prisma = new PrismaClient()
 
 export default class ActivityController {
     async create(request: Request, response: Response) {
-        
+
         const createActivity = z.object({
-            name:      z.string(),
-            details:   z.string(),
-            type:      z.string(),
+            name: z.string(),
+            details: z.string(),
+            type: z.string(),
             kilometer: z.number(),
-            effort:    z.number(),
-            
+            effort: z.number(),
+
         })
 
         const { name, details, type, kilometer, effort } = createActivity.parse(request.body)
 
         await prisma.sports_activity.create({
             data: {
-                name, details, type, kilometer, effort                
+                name, details, type, kilometer, effort
             }
         })
 
         return response.status(200).send({ success: "Sucesso" })
+    }
+
+    async readAll(request: Request, response: Response) {
+        const allRead = await prisma.sports_activity.findMany()
+
+
+        return response.status(200).send(allRead)
+    }
+    async read(request: Request, response: Response) {
+        const readUnique = await prisma.sports_activity.findUnique({
+            where: {
+                id: Number(request.params.id),
+            },
+        })
+        return response.status(200).send(readUnique)
+    }
+    async update(request: Request, response: Response) {
+        const id = Number(request.params.id)
+        const createActivity = z.object({
+            name: z.string(),
+            details: z.string(),
+            type: z.string(),
+            kilometer: z.number(),
+            effort: z.number(),
+        })
+
+        const { name, details, type, kilometer, effort } = createActivity.parse(request.body)
+
+        const update = await prisma.sports_activity.update({
+            where: {
+                id: id
+            },
+            data: {
+                name, details, type, kilometer, effort
+            }
+        })
+        return response.status(200).send(update)
+
+    }
+    async delete(request: Request, response: Response) {
+        await prisma.sports_activity.delete({
+            where: {
+                id: Number(request.params.id),
+            },
+        })
+        return response.status(200).send("Deletado com sucesso")
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,9 @@ const routes = express.Router()
 const activityController = new ActivityController();
 
 routes.post("/activity", activityController.create)
+routes.get("/activity", activityController.readAll)
+routes.get("/activity/:id", activityController.read)
+routes.put("/activity/:id", activityController.update)
+routes.delete("/activity/:id", activityController.delete)
 
 export default routes;
